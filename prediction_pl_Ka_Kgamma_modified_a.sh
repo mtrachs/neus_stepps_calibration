2a7ccac86b8c123879d61bd57a28bd8d58b82774
mtrachs <mtrachs@gmail.com>
2018-04-09 07:57:40 -0500

    Shell files

--- prediction_pl_Ka_Kgamma_modified_a.sh
+++ prediction_pl_Ka_Kgamma_modified_a.sh
@@ -8,6 +8,6 @@
 #$ -l gpu=1
 setenv OMP_NUM_THREADS 23
 
-./pred_stepps_veg_no_time sample num_samples=2000 num_warmup=500 data file=prediction_15_taxa_6796_cells_260_knots_cal_pl_Ka_Kgamma_EPs_modified_a_88_sites_only_abies_half_rho.dump output file=prediction_pl_Ka_Kgamma_modified_a_88_sites_nb_half_rho.csv
+./pred_stepps_veg_no_time sample num_samples=2000 num_warmup=500 data file=prediction_15_taxa_6796_cells_260_knots_cal_pl_Ka_Kgamma_EPs_modified_a_88_sites_only_abies.dump output file=prediction_pl_Ka_Kgamma_modified_a_88_sites_nb.csv
 
 
